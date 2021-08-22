6c6cd579f87f1caf2ba1fff1866846476cba2305
balao1312 <balao1014@hotmail.com>
2021-08-22 16:58:41 +0800

    basic done

--- auto_input_core.py
+++ auto_input_core.py
@@ -2,7 +2,9 @@ import threading
 import pyautogui
 from time import sleep
 from datetime import datetime
+from datetime import timedelta
 import subprocess
+
 
 def copy_osx(text):
     p = subprocess.Popen(['pbcopy', 'w'],
@@ -10,34 +12,49 @@ def copy_osx(text):
     p.communicate(input=text.encode('utf-8'))
 
 
-def paste_osx():
-    p = subprocess.Popen(['pbpaste', 'r'],
-                         stdout=subprocess.PIPE, close_fds=True)
-    stdout, stderr = p.communicate()
-    return stdout.decode('utf-8', pos=(
-            30, 120),)
+def test():
+    delta = timedelta(seconds=5)
+    send_time = datetime.now() + delta
+    start_auto_input('測試文字', send_time, 5)
 
-def start_auto_input(content, send_time, delay, repeat_time):
+
+def start_auto_input(content, send_time, repeat_time):
+
+    print('-' * 50)
+    print(
+        f'send time: {send_time}\nrepeat times: {repeat_time}')
+    print('-' * 50)
+
     t = threading.current_thread()
     print('start auto input ...')
     copy_osx(content)
+    sleep(1)
 
-    # init pyautogui for later use due to delay problem.
+    # init pyautogui for later use for no preloading time.
+    pyautogui.press('shift')
+    sleep(0.1)
     pyautogui.press('shift')
 
+    count = 0
     while getattr(t, 'do_run', True):
-        print('loop start')
-        if datetime.now().second % 10 == 5:
-            pyautogui.hotkey('command', 'v')
+        count += 1
+        print('loop checkpoint, seq=', count)
 
+        # standby at applied_time - 3s
+        buffer_time = timedelta(seconds=3)
+        if send_time - datetime.now() < buffer_time:
             while 1:
-                if datetime.now().second % 10 == 0:
-                    press_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                    pyautogui.press('enter')
-                    sent_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                    print(
-                        f'msg pressed enter at {press_time}, sent at {sent_time}')
+                if datetime.now() > send_time:
+                    for _ in range(repeat_time):
+                        press_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                        pyautogui.hotkey('command', 'v', interval=0.03)
+                        pyautogui.press('enter')
+                        sent_done_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                        print(
+                            f'pressed enter at {press_time}, sent at {sent_done_time}')
                     break
+            break
+
         sleep(1)
 
-    print('Auto input stoped.')
+    print('Session end, enter standby.')
--- auto_input_gui.py
+++ auto_input_gui.py
@@ -1,10 +1,14 @@
 import wx
 from auto_input_core import start_auto_input
+from auto_input_core import test
 import threading
+from time import sleep
+from datetime import timedelta
+import datetime
 
 
 class Auto_Input(wx.Frame):
-    is_runnig = False
+    is_running = False
 
     def __init__(self, parent, title):
         super(Auto_Input, self).__init__(parent, title=title)
@@ -31,9 +35,9 @@ class Auto_Input(wx.Frame):
         fgs = wx.FlexGridSizer(4, 2, 15, 25)
 
         content = wx.StaticText(panel, label="送出文字：")
-        send_time = wx.StaticText(panel, label="送出時間：")
+        send_time = wx.StaticText(panel, label="送出基準時間：")
         delay = wx.StaticText(panel, label="增加延遲(ms)：")
-        repeat_time = wx.StaticText(panel, label="送出次數：")
+        repeat_time = wx.StaticText(panel, label="重覆送出次數：")
 
         self.tc_content = wx.TextCtrl(panel)
         self.tc_send_time = wx.TextCtrl(panel)
@@ -53,44 +57,138 @@ class Auto_Input(wx.Frame):
         hbox.Add(fgs, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
         panel.SetSizer(hbox)
 
-        self.button = wx.Button(panel, wx.ID_ANY, pos=(
-            150, 180), size=(200, 20), label="開始執行")
+        # start button
+        self.button_start = wx.Button(panel, wx.ID_ANY, pos=(
+            90, 180), size=(150, 20), label="開始執行")
+        self.button_start.Bind(wx.EVT_BUTTON, self.start_button_pressed)
 
-        self.button.Bind(wx.EVT_BUTTON, self.OnButtonClicked)
+        # stop button
+        self.button_stop = wx.Button(panel, wx.ID_ANY, pos=(
+            280, 180), size=(150, 20), label="停止執行")
+        self.button_stop.Bind(wx.EVT_BUTTON, self.stop_button_pressed)
 
-        self.SetSize((500, 250))
+        # test button
+        # self.button_test = wx.Button(panel, wx.ID_ANY, pos=(
+        #     400, 180), size=(80, 20), label="測試")
+        # self.button_test.Bind(wx.EVT_BUTTON, self.OnButtonClicked_test)
+
+        # output section
+        self.output = wx.StaticText(panel, pos=(30, 230), label='')
+
+        self.SetSize((500, 400))
         self.Centre()
 
     def OnQuit(self, e):
+        try:
+            self.th.do_run = False
+        except:
+            pass
         self.Close()
 
-    def OnButtonClicked(self, e):
-        if not self.tc_content.GetValue() or \
-                not self.tc_send_time.GetValue() or \
-                not self.tc_delay.GetValue() or \
-                not self.tc_repeat_time.GetValue():
-            print('input value missing.')
+    def validate_input(self):
+        # content
+        content = self.tc_content.GetValue()
+        if not content:
+            self.output.SetLabel('錯誤：資料欄位-送出文字 空白')
+            return False
+
+        # base_time
+        base_time = self.tc_send_time.GetValue()
+        if not base_time:
+            self.output.SetLabel('錯誤：資料欄位-送出基準時間 空白')
+            return False
+        # time format check
+        try:
+            base_time_object = datetime.datetime.strptime(
+                base_time, "%Y-%m-%d %H:%M:%S")
+        except Exception as e:
+            self.output.SetLabel('送出基準時間格式有誤，ex: 2021-08-23 08:05:00')
+            return False
+        # time in future
+        past_time_delta = timedelta(seconds=0)
+        if base_time_object - datetime.datetime.now() < past_time_delta:
+            self.output.SetLabel('送出基準時間有誤，目標時間已過')
+            return False
+
+        # delay
+        delay = self.tc_delay.GetValue()
+        if not delay:
+            self.output.SetLabel('錯誤：資料欄位-增加延遲 空白')
+            return False
+        try:
+            delay = int(delay)
+        except:
+            self.output.SetLabel('增加延遲資料有誤,請輸入數字')
+            return False
+
+        # repeat time
+        repeat_time = self.tc_repeat_time.GetValue()
+        if not repeat_time:
+            self.output.SetLabel('錯誤：資料欄位-重覆送出次數 空白')
+            return False
+        try:
+            repeat_time = int(repeat_time)
+        except:
+            self.output.SetLabel('重覆送出次數資料有誤,請輸入數字')
+            return False
+
+        send_time = base_time_object + timedelta(milliseconds=delay)
+        return (content, send_time, repeat_time)
+
+    def start_button_pressed(self, e):
+        if self.validate_input():
+            content, sendtime, repeat_time = self.validate_input()
+            self.confirm_msg = f'送出內容： {content}\n' \
+                f'送出時間： {sendtime}\n' \
+                f'送出重覆次數： {repeat_time}'\
+                f'\n-------------------------------\n'\
+                f'等待目標時間到達 ...'
+            self.output.SetLabel(self.confirm_msg)
+        else:
             return
 
-        if self.is_runnig:
-            self.th.do_run = False
-            self.button.SetLabel('開始執行')
-            self.is_runnig = False
-        else:
-            print("送出文字", self.tc_content.GetValue())
-            print("送出時間", self.tc_send_time.GetValue())
-            print("增加延遲", self.tc_delay.GetValue())
-            print("送出次數", self.tc_repeat_time.GetValue())
-            self.button.SetLabel('結束執行')
-            self.is_runnig = True
+        # fire up
+        self.th = threading.Thread(
+            target=start_auto_input, args=(content, sendtime, repeat_time))
+        self.th.start()
 
-            # must multi threading
-            self.th = threading.Thread(target=start_auto_input, args=(
-                self.tc_content.GetValue(),
-                self.tc_send_time.GetValue(),
-                self.tc_delay.GetValue(),
-                self.tc_repeat_time.GetValue()))
-            self.th.start()
+        self.th_check_done = threading.Thread(
+            target=self.check_done, args=())
+        self.th_check_done.start()
+
+        self.is_running = True
+
+    def stop_button_pressed(self, e):
+        try:
+            self.th.do_run = False
+        except:
+            pass
+        self.output.SetLabel('已停止')
+        self.is_running = False
+
+    def OnButtonClicked_test(self, e):
+        msg = '等待目標時間...'
+        self.output.SetLabel(msg)
+        self.th = threading.Thread(target=test, args=())
+        self.th.start()
+
+        self.th_check_done = threading.Thread(
+            target=self.check_done, args=())
+        self.th_check_done.start()
+
+    def check_done(self):
+        while 1:
+            if not self.th.is_alive():
+                self.confirm_msg += f'\n{"-"*50}'
+                self.confirm_msg += '\n完成送出!'
+                try:
+                    if self.is_running:
+                        self.output.SetLabel(self.confirm_msg)
+                    self.is_running = False
+                except:
+                    pass
+                break
+            sleep(1)
 
 
 def main():
