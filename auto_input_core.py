85d78b41c7f50197976e91e67bb0f85d2c81171a
balao1312 <balao1014@hotmail.com>
2021-08-25 22:10:51 +0800

    add button to input test values; add more textboxes; layout restructured.

--- auto_input_core.py
+++ auto_input_core.py
@@ -12,20 +12,24 @@ import pyperclip
 def test():
     delta = timedelta(seconds=5)
     send_time = datetime.now() + delta
-    start_auto_input('測試文字', send_time, 5)
+    start_auto_input(['1.1\n1.2\n1.3', '2.1\n2.2\n2.3', '3.1\n3.2\n3.3'], send_time, 2)
 
 
-def start_auto_input(content, send_time, repeat_time):
+def start_auto_input(content_list, send_time, repeat_time):
 
     print('-' * 50)
     print(
         f'send time: {send_time}\nrepeat times: {repeat_time}')
     print('-' * 50)
 
+    # thread object to get 'do_run' key to get stop signal from main process.
     t = threading.current_thread()
+
     print('start session ...')
-    pyperclip.copy(content)
-    sleep(1)
+
+    # msg format
+    # pyperclip.copy(content_1)
+    # sleep(1)
 
     # init pyautogui for later use for no preloading time.
     pyautogui.press('ctrl')
@@ -43,12 +47,14 @@ def start_auto_input(content, send_time, repeat_time):
             while 1:
                 if datetime.now() > send_time:
                     for _ in range(repeat_time):
-                        press_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                        pyautogui.hotkey('command', 'v', interval=0.03)
-                        pyautogui.press('enter')
-                        sent_done_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
-                        print(
-                            f'pressed enter at {press_time}, sent at {sent_done_time}')
+                        # press_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                        for each in content_list:
+                            pyperclip.copy(each)
+                            pyautogui.hotkey('command', 'v', interval=0.015)
+                            pyautogui.press('enter')
+                        # sent_done_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+                        # print(
+                            # f'pressed enter at {press_time}, sent at {sent_done_time}')
                     break
             break
 
--- auto_input_gui.py
+++ auto_input_gui.py
@@ -1,5 +1,5 @@
-#! /usr/bin/env python3 
-##-*- coding: utf-8 -*-
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import wx
 from auto_input_core import start_auto_input
@@ -33,52 +33,81 @@ class Auto_Input(wx.Frame):
 
         panel = wx.Panel(self)
 
-        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        # -------------------------------------------------------------
 
-        fgs = wx.FlexGridSizer(4, 2, 15, 25)
+        # main input section
+        fgs = wx.FlexGridSizer(7, 2, 15, 25)
 
-        content = wx.StaticText(panel, label="送出文字：")
+        content_1 = wx.StaticText(panel, label="送出文字 1：")
+        content_2 = wx.StaticText(panel, label="送出文字 2：")
+        content_3 = wx.StaticText(panel, label="送出文字 3：")
         send_time = wx.StaticText(panel, label="送出基準時間：")
         delay = wx.StaticText(panel, label="增加延遲(ms)：")
         repeat_time = wx.StaticText(panel, label="重覆送出次數：")
 
-        self.tc_content = wx.TextCtrl(panel)
+        self.tc_content_1 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        self.tc_content_2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        self.tc_content_3 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
         self.tc_send_time = wx.TextCtrl(panel)
         self.tc_delay = wx.TextCtrl(panel)
         self.tc_repeat_time = wx.TextCtrl(panel)
 
-        fgs.AddMany([
-                    (content), (self.tc_content, 1, wx.EXPAND),
-                    (send_time), (self.tc_send_time, 1, wx.EXPAND),
-                    (delay), (self.tc_delay, 1, wx.EXPAND),
-                    (repeat_time), (self.tc_repeat_time, 1, wx.EXPAND),
-                    ])
-
         # fgs.AddGrowableRow(2, 1)
         fgs.AddGrowableCol(1, 1)
 
-        hbox.Add(fgs, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
-        panel.SetSizer(hbox)
+        fgs.AddMany([
+            (content_1), (self.tc_content_1, 1, wx.EXPAND),
+            (content_2), (self.tc_content_2, 1, wx.EXPAND),
+            (content_3), (self.tc_content_3, 1, wx.EXPAND),
+            (send_time), (self.tc_send_time, 1, wx.EXPAND),
+            (delay), (self.tc_delay, 1, wx.EXPAND),
+            (repeat_time), (self.tc_repeat_time, 1, wx.EXPAND),
+        ])
+        # -------------------------------------------------------------
+
+        # horizontal section
+        fgs_button = wx.FlexGridSizer(1, 4, 115, 25)
+
+        # get time button
+        self.button_get_default = wx.Button(panel, wx.ID_ANY, label="輸入範本")
+        self.button_get_default.Bind(wx.EVT_BUTTON, self.get_default)
 
         # start button
-        self.button_start = wx.Button(panel, wx.ID_ANY, pos=(
-            90, 180), size=(150, 20), label="開始執行")
+        self.button_start = wx.Button(panel, wx.ID_ANY, label="開始執行")
         self.button_start.Bind(wx.EVT_BUTTON, self.start_button_pressed)
 
         # stop button
-        self.button_stop = wx.Button(panel, wx.ID_ANY, pos=(
-            280, 180), size=(150, 20), label="停止執行")
+        self.button_stop = wx.Button(panel, wx.ID_ANY, label="停止執行")
         self.button_stop.Bind(wx.EVT_BUTTON, self.stop_button_pressed)
 
         # test button
-        # self.button_test = wx.Button(panel, wx.ID_ANY, pos=(
-        #     400, 180), size=(80, 20), label="測試")
-        # self.button_test.Bind(wx.EVT_BUTTON, self.OnButtonClicked_test)
+        # self.button_test = wx.Button(panel, wx.ID_ANY, label="測試")
+        # self.button_test.Bind(wx.EVT_BUTTON, self.OnButtonClicked_test_with_fixed_value)
+
+        fgs_button.AddMany(
+            [(self.button_get_default), (self.button_start,), (self.button_stop)]
+        )
+        # -------------------------------------------------------------
+
+        # static output
+        fgs_output = wx.FlexGridSizer(1, 1, 1)
+        self.output = wx.StaticText(panel, size=(400, 300), label='')
+
+        fgs_output.AddMany(
+            [self.output]
+        )
+        # -------------------------------------------------------------
+
+        # come to mommy
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        vbox.Add(fgs, proportion=1, flag=wx.ALL | wx.EXPAND, border=15)
+        vbox.Add(fgs_button, proportion=1, flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, border=15)
+        vbox.Add(fgs_output, proportion=1, flag=wx.ALL | wx.ALIGN_CENTER_HORIZONTAL, border=15)
+        panel.SetSizer(vbox)
 
         # output section
-        self.output = wx.StaticText(panel, pos=(30, 230), label='')
 
-        self.SetSize((500, 400))
+        self.SetSize((800, 700))
         self.Centre()
 
     def OnQuit(self, e):
@@ -89,10 +118,19 @@ class Auto_Input(wx.Frame):
         self.Close()
 
     def validate_input(self):
-        # content
-        content = self.tc_content.GetValue()
-        if not content:
-            self.output.SetLabel('錯誤：資料欄位-送出文字 空白')
+        content_list = []
+
+        content_1 = self.tc_content_1.GetValue()
+        content_2 = self.tc_content_2.GetValue()
+        content_3 = self.tc_content_3.GetValue()
+
+        for i in range(1,4):
+            if locals()[f'content_{i}'] != '':
+                content_list.append(locals()[f'content_{i}'] )
+
+        print(content_list)
+        if not content_list:
+            self.output.SetLabel('錯誤：資料欄位-送出文字皆空白')
             return False
 
         # base_time
@@ -136,7 +174,7 @@ class Auto_Input(wx.Frame):
             return False
 
         send_time = base_time_object + timedelta(milliseconds=delay)
-        return (content, send_time, repeat_time)
+        return (content_list, send_time, repeat_time)
 
     def start_button_pressed(self, e):
         if self.validate_input():
@@ -144,7 +182,7 @@ class Auto_Input(wx.Frame):
             self.confirm_msg = f'送出內容： {content}\n' \
                 f'送出時間： {sendtime}\n' \
                 f'送出重覆次數： {repeat_time}'\
-                f'\n-------------------------------\n'\
+                f'\n{"-"*50}\n'\
                 f'等待目標時間到達 ...'
             self.output.SetLabel(self.confirm_msg)
         else:
@@ -169,8 +207,8 @@ class Auto_Input(wx.Frame):
         self.output.SetLabel('已停止')
         self.is_running = False
 
-    def OnButtonClicked_test(self, e):
-        msg = '等待目標時間...'
+    def OnButtonClicked_test_with_fixed_value(self, e):
+        msg = '測試...'
         self.output.SetLabel(msg)
         self.th = threading.Thread(target=test, args=())
         self.th.start()
@@ -192,6 +230,19 @@ class Auto_Input(wx.Frame):
                     pass
                 break
             sleep(1)
+    
+    def get_default(self, event):
+        self.tc_content_1.SetValue('')
+        self.tc_content_1.AppendText('1.1\n1.2\n1.3')
+        self.tc_content_2.SetValue('')
+        self.tc_content_2.AppendText('2.1\n2.2\n2.3')
+        self.tc_content_3.SetValue('')
+        self.tc_content_3.AppendText('3.1\n3.2\n3.3')
+
+        example_time = (datetime.datetime.now() + timedelta(seconds=10)).strftime("%Y-%m-%d %H:%M:%S")
+        self.tc_send_time.SetLabel(example_time)
+        self.tc_delay.SetLabel('0')
+        self.tc_repeat_time.SetLabel('3')
 
 
 def main():
